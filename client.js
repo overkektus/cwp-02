b520bbb6313796c5ed8ed3675c45864ca545cbc8
Egor Piskunou <egor.piskunou@gmail.com>
2018-03-14 10:23:10 +0300

    done

--- client.js
+++ client.js
@@ -1,54 +1,52 @@
-const fs = require('fs');
 const net = require('net');
+const fs = require('fs');
 const port = 8124;
-const host = '127.0.0.1';
-
-const clientString = 'QA';
-const good = 'ACK';
+const string = 'QA';
 const bad = 'DEC';
+const good = 'ACK';
 
 const client = new net.Socket();
-
-let questions = [];
 let currentIndex = -1;
-
 client.setEncoding('utf8');
 
-client.connect({port: port, host: host}, async () => {
-  client.write(clientString);
-  questions = await getQuestions();
-});
-
-client.on('data', async (data) => {
-  if(data === bad) client.destroy();
-  if(data === good) sendQuestion();
-  else {
-    let qst = questions[currentIndex];
-    let answer = qst.ans;
-    console.log('\n' + qst.quest);
-    console.log('Answer:' + data);
-    console.log('Server:' + answer);
-    console.log('Result:' + (data === answer ? 'It is a right answer': 'Bad answer'));
-    sendQuestion();
-  }
-});
-
-client.on('close', () => {
-  console.log('Connection closed');
-});
-
-const getQuestions = () => {
-  return new Promise((resolve, reject) => {
-    fs.readFile('./qa.json', (err, data) => {
-      resolve(JSON.parse(data));
+let questions = [];
+client.connect({port: port, host: '127.0.0.1'}, () => {
+    fs.readFile("qa.json", (err, text) => {
+        if (!err) {
+            questions = JSON.parse(text);
+            client.write(string);
+        }
+        else console.error(err);
     });
-  });
-}
+});
 
-const sendQuestion = () => {
-  if(currentIndex < questions.length - 1) {
-    let qst = questions[++currentIndex].quest;
-    client.write(qst);
-  } else
-    client.destroy();
+client.on('data', (data) => {
+    if (data === bad)
+        client.destroy();
+    if (data === good)
+        sendQuestion();
+    else {
+        let qst = questions[currentIndex];
+        let answer = qst.good;
+        console.log('\n' + qst.quest);
+        console.log('Answer:' + data);
+        console.log('Server:' + answer);
+        console.log('Result:' + (data === answer ? 'It is a right answer': 'Bad answer'));
+        sendQuestion();
+    }
+});
+
+client.on('close', function () {
+    console.log('Connection closed');
+});
+
+
+
+function sendQuestion() {
+    if (currentIndex < questions.length -1) {
+        let qst = questions[++currentIndex].quest;
+        client.write(qst);
+    }
+    else
+        client.destroy();
 }
--- server.js
+++ server.js
@@ -1,45 +1,47 @@
-const fs = require('fs');
 const net = require('net');
+const fs = require('fs');
 const port = 8124;
-
 const clientString = 'QA';
 const good = 'ACK';
 const bad = 'DEC';
-
-const logger = fs.createWriteStream('./client_id.log');
+const logger = fs.createWriteStream('client_id.log');
 
 let seed = 0;
 
 const server = net.createServer((client) => {
-  console.log('Client connected');
-  client.setEncoding('utf8');
+    console.log('Client connected');
+    client.setEncoding('utf8');
 
-  client.on('data', (data) => {
-    
-    if(data === clientString) {
-      client.id = Date.now() + seed++;
-      writeToLog('Client #' + client.id + ' connected\n');
-      client.write(good);
-    } else {
-      writeToLog('Client #' + client.id + ' has asked: ' + data + '\n');
-      let answer = generateAnswer();
-      writeToLog('Server answered to Client #' + client.id + ': ' + answer + '\n');
-      client.write(answer.toString());
-    }
-
-  });
-
-  client.on('end', () => console.log('Client disconnected'));
+    client.on('data', (data, err) =>
+    {
+        if (err) console.error(err);
+        else if (!err && data === clientString)
+        {
+            client.id = Date.now() + seed++;
+            writeLog('Client #' + client.id + ' connected\n');
+            client.write(data === clientString ? good : bad);
+        }
+        else if (!err && data !== clientString) {
+            writeLog('Client #' + client.id + ' has asked: ' + data + '\n');
+            let answer = generateAnswer();
+            writeLog('Server answered to Client #' + client.id + ': ' + answer + '\n');
+            client.write(answer);
+        }
+    });
+    client.on('end', () =>
+    {
+        logger.write('Client #'+ client.id+ ' disconnected');
+        console.log('Client disconnected')
+    });
 });
-
+function writeLog(data)
+{
+    logger.write(data);
+}
+function generateAnswer()
+{
+    return Math.random() > 0.5 ? '1' : '0';
+}
 server.listen(port, () => {
-  console.log(`Server listening on localhost:${port}`);
+    console.log(`Server listening on localhost: ${port}`);
 });
-
-const writeToLog = (data) => {
-  logger.write(data);
-}
-
-const generateAnswer = () => {
-  return Math.round(Math.random());
-}
